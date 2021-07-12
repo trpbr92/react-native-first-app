e73b73fe0be03b4665e3e3a0de4341e33c97f0f6
Trace Brandt <trpbr92@gmail.com>
2021-07-12 11:19:12 -0500

    text input styling

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{padding: 50}}>
+      <View>
+      <TextInput 
+      placeholder='Course Goal' 
+      style={{ borderColor: 'black', borderWidth: 1, padding: 10 }}/>
+      <Button title ='ADD' />
+      </View>
+      <View>
+
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+ 
 });
