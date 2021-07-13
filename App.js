ccff42d98cd986fe156cd4733727bbddfcc4f9e6
Trace Brandt <trpbr92@gmail.com>
2021-07-13 11:13:14 -0500

    goalinput and goalitem components

--- App.js
+++ App.js
@@ -1,37 +1,24 @@
 import React, {useState} from 'react';
 import { StyleSheet, Text, View, Button, TextInput, ScrollView, FlatList } from 'react-native';
 
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
+
+
 export default function App() {
-const [enteredGoal, setEnteredGoal] = useState('');
 const [courseGoals, setCourseGoals] = useState([]);
 
-const handleGoalInput = (enteredText) => {
-  setEnteredGoal(enteredText);
-};
-
-const handleAddGoal = () => {
-  setCourseGoals(currentGoals => [...currentGoals, {id: Math.random().toString(), value: enteredGoal}]);
+const handleAddGoal = goalTitle => {
+  setCourseGoals(currentGoals => [...currentGoals, {id: Math.random().toString(), value: goalTitle }]);
 };
 
   return (
     <View style={styles.screen}>
-      <View style={styles.inputContainer}>
-        <TextInput 
-        placeholder='Course Goal' 
-        style={styles.input} 
-        onChangeText={handleGoalInput}
-        value={enteredGoal}
-        />
-        <Button title ='ADD' onPress={handleAddGoal} />
-      </View>
+      <GoalInput onAddGoal={handleAddGoal} />
       <FlatList
       keyExtractor={(item, index) => item.id} 
       data={courseGoals} 
-      renderItem={itemData => (
-      <View style={styles.listItem}>
-      <Text>{itemData.item.value}</Text>
-    </View> 
-      )} 
+      renderItem={itemData => <GoalItem title={itemData.item.value} /> } 
     />
     </View>
   );
@@ -40,23 +27,5 @@ const handleAddGoal = () => {
 const styles = StyleSheet.create({
  screen: {
    padding: 50
- },
- inputContainer: {
-  flexDirection: 'row', 
-  justifyContent: 'space-between', 
-  alignItems: 'center'
- },
-input: {
-  width: '80%', 
-  borderColor: 'black', 
-  borderWidth: 1, 
-  padding: 10
-},
-listItem: {
-  padding: 10,
-  marginVertical: 10,
-  backgroundColor: '#ccc',
-  borderColor: 'black',
-  borderWidth: 1
-}
+ }
 });
