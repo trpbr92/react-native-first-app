6bcffe3edefa93b30129b88b297181eba16b3724
Trace Brandt <trpbr92@gmail.com>
2021-07-13 11:33:06 -0500

    delete function working

--- App.js
+++ App.js
@@ -12,13 +12,19 @@ const handleAddGoal = goalTitle => {
   setCourseGoals(currentGoals => [...currentGoals, {id: Math.random().toString(), value: goalTitle }]);
 };
 
+const handleRemoveGoal = goalId => {
+  setCourseGoals(currentGoals => {
+    return currentGoals.filter((goal) => goal.id !== goalId);
+  });
+}
+
   return (
     <View style={styles.screen}>
       <GoalInput onAddGoal={handleAddGoal} />
       <FlatList
       keyExtractor={(item, index) => item.id} 
       data={courseGoals} 
-      renderItem={itemData => <GoalItem title={itemData.item.value} /> } 
+      renderItem={itemData => <GoalItem id={itemData.item.id} onDelete={handleRemoveGoal} title={itemData.item.value} /> } 
     />
     </View>
   );
